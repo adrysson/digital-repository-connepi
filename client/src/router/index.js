9d4aecf0d5d94bf6d3c62c396ecf32de7b741189
Adrysson <adrysson_santos@msn.com>
2018-09-22 00:29:37 -0300

    Adicionando template

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Index from '@/components/Index'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Index',
+      component: Index
     }
   ]
 })
